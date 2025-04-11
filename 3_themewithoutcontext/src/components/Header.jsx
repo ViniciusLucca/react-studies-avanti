41e07daff0fb79f4a8aef689fc3af273142079ea
ViniciusLucca <vinilucca7304@gmail.com>
2025-04-11 14:51:02 -0300

    update header at project 3
    This will create create an object with two properties as an argument to the constructor, and assign the second argument to the button onClick event listener.

--- 3_themewithoutcontext/src/components/Header.jsx
+++ 3_themewithoutcontext/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({theme, toggleTheme}) => {
   return (
-    <div>Header</div>
+    <header>
+        <h1>App Theme:</h1>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+    </header>
   )
 }
 
